70eea1c328b95b5694bd10a41a62af95a2b4ae14
riceboyyyyyyy <147307211+riceboyyyyyyy@users.noreply.github.com>
2023-11-23 16:12:19 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ imgs = ["guy (1).jpg", "guy (1).PNG", "guy (2).jpg", "guy (2).PNG", "guy (3).jpg
         "guy (8).PNG", "guy (9).jpg", "guy (9).PNG", "guy (10).jpg", "guy (11).jpg", 
         "guy (12).jpg", "guy (13).jpg", "guy (14).jpg", "guy (15).JPG", "guy (16).JPG", 
         "guy (16).JPG", "guy (17).JPG", "guy (18).jpg", "pic (5).PNG", "pic (10).PNG", 
-        "guy new (1).JPG", "guy new (2).JPG", "guy new (3).JPG", "guy new (4).JPG", "guy new (5).JPG", 
-       "guy new (6).JPG", "guy new (7).JPG", "guy new (8).JPG", "guy new (9).JPG", "guy new (10).JPG", 
-       "guy new (11).JPG", "guy new (12).JPG", "guy new (13).JPG", "guy new (14).JPG", "guy new (15).JPG", 
-       "guy new (16).JPG", "guy new (17).JPG", "guy new (18).JPG"]
+        "guy new (1).jpg", "guy new (2).jpg", "guy new (3).jpg", "guy new (4).jpg", "guy new (5).jpg", 
+       "guy new (6).jpg", "guy new (7).jpg", "guy new (8).jpg", "guy new (9).jpg", "guy new (10).jpg", 
+       "guy new (11).jpg", "guy new (12).jpg", "guy new (13).jpg", "guy new (14).jpg", "guy new (15).jpg", 
+       "guy new (16).JPG", "guy new (17).jpg", "guy new (18).jpg"]
 
 msgs = ["u got this!!!", "i love u <33", "i always believe in u", "no giving up!!", 
         "work hard and u can have loki", "im very proud of u no matter what", 
